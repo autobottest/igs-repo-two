cfa52381d54d9efc5c4221f10648908059cb1ba1
TanTran <ttran4@atlassian.com>
2024-09-24 09:38:52 +1000

    KAN-2

--- secret.js
+++ secret.js
@@ -1,1 +1,2 @@
-export const FAKE_SECRET_TOKEN = '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';
+export const FAKE_SECRET = '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';
+ to move to the next interative element on the page.
