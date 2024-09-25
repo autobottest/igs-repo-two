039bbf71a0440afe11993ce3c250d7a349721c77
TanTran <ttran4@atlassian.com>
2024-09-25 12:08:15 +1000

    Update sql-injection.js

--- sql-injection.js
+++ sql-injection.js
@@ -2,13 +2,13 @@ var express = require("express");
 
 var app = express();
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("database", "fakeUser", "fakePassword", {
+const sequelize = new ("database", "fakeUser", "fakePassword", {
   dialect: "sqlite",
   storage: "data/fakeDB.sqlite",
 });
 
-app.post("/fakeLogin", function (req, res) {
-  sequelize.query(
+app.("/fakeLogin", function (req, res) {
+  sequelize.(
     "SELECT * FROM Products WHERE name LIKE " + req.body.username
   );
   res.write(req.body.xss)
